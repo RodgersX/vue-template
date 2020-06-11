abe2f6641796e5498e6dc4406af21adf0e32bb5b
RodgersX <brianmawira2@gmail.com>
2020-06-11 17:51:53 +0300

    final commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(Router)
 
@@ -12,11 +12,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('./views/About.vue')
     }
   ]
 })
